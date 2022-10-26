ce7f826ed583ba872f17c54695ff3fbcfb5aedd1
asiripragada <92725571+asiripragada@users.noreply.github.com>
2022-10-26 09:49:48 +0000

    Update settings.py

--- newapi/settings.py
+++ newapi/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =   'django-insecure-irbte538m)xm+0aox1lhb!t$jkih$b7px%nc*1__8z1!uo-n=2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
